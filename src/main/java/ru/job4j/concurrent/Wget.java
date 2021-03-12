221e85186af25930b69ad1a52c121759ee357831
ildar-bayt <zicmdi@gmail.com>
2021-03-12 15:55:57 +0300

    1.2. Режим ожидания.

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int percent = 0; percent <= 100; percent++) {
+                            System.out.print("\rLoading : " + percent  + "%");
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
