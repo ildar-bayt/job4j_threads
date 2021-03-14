502ec3fde7e4f5e2da863fe1d199615bd41a87c1
ildar-bayt <zicmdi@gmail.com>
2021-03-14 15:04:58 +0300

    3.1. Прерывание блокированной нити.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,22 @@
 package ru.job4j.concurrent;
 
 public class Wget {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
                     try {
-                        for (int percent = 0; percent <= 100; percent++) {
-                            System.out.print("\rLoading : " + percent  + "%");
+                        for (int percent = 0; percent <= 100 || !Thread.currentThread().isInterrupted(); percent++) {
+                            System.out.print("\rLoading : " + percent + "%");
                             Thread.sleep(1000);
                         }
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
         thread.start();
+        Thread.sleep(10000);
+        thread.interrupt();
+        thread.join();
     }
 }
