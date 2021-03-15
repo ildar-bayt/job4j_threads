656679c129c4b3fc1960a745100a3f2e1eb9de7d
ildar-bayt <zicmdi@gmail.com>
2021-03-16 00:05:40 +0300

    2. Модель памяти Java

---
+++ src/main/java/ru/job4j/memory/DCLSingleton.java
@@ -0,0 +1,20 @@
+package ru.job4j.memory;
+
+public final class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+
+    private DCLSingleton() {
+    }
+}
